6d2fbfe0d16b11bb4a3da259b69572920d0f6180
sanchar127 <msan53366@gmail.com>
2024-10-30 22:39:53 +0545

    implementing lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 
 export default function Home() {
-  // const a =0 
-  // a=''
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -22,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your change instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
